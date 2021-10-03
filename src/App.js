6a0d146cb0624753e96b8519675b8493c3ad4101
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-10-03 16:21:39 -0600

    Added authentication component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
+import HiddenMessage from "./components/HiddenMessage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <BookList />
+      <HiddenMessage>
+        <p>Hello World</p>
+      </HiddenMessage>
     </div>
   );
 }
