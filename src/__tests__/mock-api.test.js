ab40c1aed67474cc1b63f4006790e8d33c2ae473
Alexander Govea <alexander_alatorre1993@hotmail.com>
2021-10-04 11:12:44 -0600

    Added MockAPI tests

---
+++ src/__tests__/mock-api.test.js
@@ -0,0 +1,4 @@
+import React from "react";
+import SeachComp, { Search } from "../components/Search";
+import { render, screen, fireEvent, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
